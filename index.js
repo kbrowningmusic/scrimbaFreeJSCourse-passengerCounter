b1170412f381c55899f0ff32a5419dc40888e018
Kat Browning <kbrowning.music@gmail.com>
2022-10-16 18:27:25 +0100

    First Lessons - counters, vars and console.log
    Went into some info about using console.log in your browser - super useful for testing code!
    
    I've mostly commented everything in !

---
+++ index.js
@@ -0,0 +1,15 @@
+//document.getElementById("count").innerText = 5
+//above hard-codes the count to show as 5
+
+// let count = 0
+// console.log(count)
+
+//here scrimba explained how you can use the console.log
+//function in your browser to test your code
+//right-click->inspect->click console, write your code
+//console.log it
+//NB - console log needs to be AFTER you define the variable
+//otherwise it will say either undefined 
+
+let myAge = 34;
+console.log(myAge)
